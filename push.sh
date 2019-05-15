2c92102ffeabc908812fea25f979cb56f14ceea4
binlidaily <binlidaily@gmail.com>
2019-05-15 13:51:22 +0800

    052

--- push.sh
+++ push.sh
@@ -1,3 +1,39 @@
+#!/bin/sh
+COMMENT=$1
+if [ -z "$COMMENT" ];
+then
+	name1='commit'
+fi 
+
+
+# name1="$1"
+# echo "local files: $name1"
+# if [ -z "$name1" ];
+# then
+# 	name1=*
+# else
+# 	if [ "$name1"==*/ ];
+# 	then
+# 		name1=$name1*
+# 	fi
+# fi
+
+# name2=$2
+# echo "remote files:" $name2
+# if [ -z "$name2" ];
+# then
+# 	name2=""
+# else
+# 	if [ "$name2"==*/ ];
+# 	then
+# 		name2=$name2
+# 	fi
+# fi
+# echo scp -r ./$name1 binli@192.168.31.127:/home/binli/repos/cayman/$name2
+
+# scp -r ./$name1 binli@192.168.31.127:/home/binli/repos/cayman/$name2
+
+
 git add -A
-git commit -m 'commit'
+git commit -m $COMMENT
 git push
